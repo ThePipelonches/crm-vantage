21f0cf0336c379d02f9af035891a55882a3210a4
ThePipelonches <andresclinicapsicologica@gmail.com>
2026-05-26 11:28:42 -0500

    fix: Add missing 'title' prop to TopBar component

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -10,10 +10,10 @@ export function AppLayout() {
 
       {/* Contenedor Principal */}
       <div className="flex-1 flex flex-col min-w-0 lg:ml-64 transition-all duration-300 ease-in-out">
-        {/* Barra Superior (Solo mÃ³vil o para bÃºsqueda/perfil) */}
-        <TopBar />
+        {/* Barra Superior (Solo mÃƒÂ³vil o para bÃƒÂºsqueda/perfil) */}
+        <TopBar title={''} />
         
-        {/* Ãrea de Contenido Principal */}
+        {/* ÃƒÂrea de Contenido Principal */}
         <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8 scroll-smooth">
           <Outlet />
         </main>
